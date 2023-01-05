a0ce18cc1e2fed4ff080565f0c7b3a2410e6435e
Vitaliy Sheyanov <vit.onix@gmail.com>
2023-01-05 17:52:35 +0300

    update dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,18 +2,18 @@ plugins {
     id("com.android.library")
     id("kotlin-android")
     id("kotlin-kapt")
-    kotlin("plugin.serialization") version "1.6.21"
+    kotlin("plugin.serialization") version "1.8.0"
     `maven-publish`
 }
 
 android {
 
-    compileSdk = 32
-    buildToolsVersion = "32.0.0"
+    compileSdk = 33
+    buildToolsVersion = "33.0.1"
 
     defaultConfig {
         minSdk = 23
-        targetSdk = 32
+        targetSdk = 33
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
@@ -59,37 +59,37 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.2.2")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
 //    implementation("com.google.android.material:material:1.6.1")
 
-    implementation("com.github.v170nix:arwix-common-library:0.4.0")
+    implementation("com.github.v170nix:arwix-common-library:0.4.1")
 
-    implementation("androidx.room:room-runtime:2.5.0-alpha02")
-    kapt("androidx.room:room-compiler:2.5.0-alpha02")
-    implementation("androidx.room:room-ktx:2.5.0-alpha02")
+    implementation("androidx.room:room-runtime:2.5.0-rc01")
+    kapt("androidx.room:room-compiler:2.5.0-rc01")
+    implementation("androidx.room:room-ktx:2.5.0-rc01")
 
 //    implementation("androidx.work:work-runtime-ktx:2.7.1")
 
-    implementation("io.ktor:ktor-client-core:2.0.3")
-    implementation("io.ktor:ktor-client-okhttp:2.0.3")
-    implementation("io.ktor:ktor-client-content-negotiation:2.0.3")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.0.3")
+    implementation("io.ktor:ktor-client-core:2.2.2")
+    implementation("io.ktor:ktor-client-okhttp:2.2.2")
+    implementation("io.ktor:ktor-client-content-negotiation:2.2.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.2")
 //    implementation("io.ktor:ktor-client-serialization-jvm:2.0.3")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 group = "ui.index.lib"
-version = "1.0.7"
+version = "1.0.8"
 
 
 
